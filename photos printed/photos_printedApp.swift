0a03ebfff83fa2c167f6ece22795cd506c97d7cb
Lilly <lillycat332@gmail.com>
2021-10-15 10:31:09 +0100

    Initial Commit

---
+++ photos printed/photos_printedApp.swift
@@ -0,0 +1,17 @@
+//
+//  photos_printedApp.swift
+//  photos printed
+//
+//  Created by Lilly Cham on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct photos_printedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
