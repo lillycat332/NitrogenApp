1a54acfa0ef768bf522c3b5755cf76a90f5b029f
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-01-28 01:54:25 +0000

    coment

--- Nitrogen/Views/DeviceView.swift
+++ Nitrogen/Views/DeviceView.swift
@@ -8,7 +8,8 @@
 import Foundation
 import SwiftUI
 
-struct FormView : View {
+struct DeviceDataView : View {
+  // parses the variables into hstack for form
   @State var label: String = ""
   @State var symbol: String = ""
   @State var value: String = ""
@@ -25,21 +26,22 @@ struct FormView : View {
 }
 
 struct DeviceView: View {
+  // main view
   var body: some View {
     Form {
       Section(header: Text("Hardware Info"), footer: Text("Hardware refers to the physical components of your device.")) {
-        FormView(label: "Model", symbol: "ipad.and.iphone", value: "\(modelName) (\(machineName()))")
-        FormView(label: "SoC", symbol: "cpu.fill", value: "\(totalCores) Core \(CPUinfoStr)")
-        FormView(label: "RAM", symbol: "memorychip.fill", value: "\(totalMem) GiB")
-        FormView(label: "Storage Size", symbol: "internaldrive.fill", value: "\(totalDiskGB)")
-        FormView(label: "Device is Simulator", symbol: "laptopcomputer", value: "\(isSimulator)")
+        DeviceDataView(label: "Model", symbol: "ipad.and.iphone", value: "\(modelName) (\(machineName()))")
+        DeviceDataView(label: "SoC", symbol: "cpu.fill", value: "\(totalCores) Core \(CPUinfoStr)")
+        DeviceDataView(label: "RAM", symbol: "memorychip.fill", value: "\(totalMem) GiB")
+        DeviceDataView(label: "Storage Size", symbol: "internaldrive.fill", value: "\(totalDiskGB)")
+        DeviceDataView(label: "Device is Simulator", symbol: "laptopcomputer", value: "\(isSimulator)")
       }
       
       Section(header: Text("Software info"), footer: Text("Software refers to the programs and systems that run on your device.")) {
-        FormView(label: "OS", symbol: "folder.fill.badge.gearshape", value: "\(OSVer)")
-        FormView(label: "Kernel", symbol: "hammer.fill", value: "\(sysName()) \(kernelVersion())")
-        FormView(label: "Uptime", symbol: "clock.fill", value: "\(uptimeParsed)")
-        FormView(label: "Hostname", symbol: "network", value: hostName)
+        DeviceDataView(label: "OS", symbol: "folder.fill.badge.gearshape", value: "\(OSVer)")
+        DeviceDataView(label: "Kernel", symbol: "hammer.fill", value: "\(sysName()) \(kernelVersion())")
+        DeviceDataView(label: "Uptime", symbol: "clock.fill", value: "\(uptimeParsed)")
+        DeviceDataView(label: "Hostname", symbol: "network", value: hostName)
       }
     }
     .navigationTitle("Device")
